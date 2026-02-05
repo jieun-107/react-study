04efa0f1b8f7beeddb12a9e490310131f4240ff7
jelee <lje7128@mteletec.com>
2026-02-05 09:35:27 +0900

    벨리데이션

--- src/components/section10/Ex01.tsx
+++ src/components/section10/Ex01.tsx
@@ -1,12 +1,52 @@
 import useInput from "../../hooks/useInput";
+import {
+  validateEmail,
+  validateName,
+  validatePassword,
+} from "../../utils/validation";
 
 export default function Ex01() {
-  const { value: email, handleValueChange: handleEmailChange } = useInput("");
-  const { value: password, handleValueChange: handlePasswordChange } = useInput("");
-  const { value: name, handleValueChange: handleNameChange } = useInput("");
+  const {
+    value: email,
+    handleValueChange: handleEmailChange,
+    error: emailError,
+  } = useInput("", validateEmail);
+  const {
+    value: password,
+    handleValueChange: handlePasswordChange,
+    error: passwordError,
+  } = useInput("", validatePassword);
+  const {
+    value: name,
+    handleValueChange: handleNameChange,
+    error: nameError,
+  } = useInput("", validateName);
+
+  // const validateEmail = (email: string) => {
+  //   if(!email) return "이메일을 입력해주세요"
+  //   if(!/^[a-zA-Z0-9._%+-]+@(gmail\.com|naver\.com)$/.test(email)) return "이메일에는 gmail과 naver만 포함할 수 있습니다."
+  // }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    // 이메일 검증
+    if (emailError) {
+      alert(emailError);
+      return;
+    }
+
+    // 비밀번호 검증
+    if (passwordError) {
+      alert(passwordError);
+      return;
+    }
+
+    // 이름 검증
+    if (nameError) {
+      alert(nameError);
+      return;
+    }
     console.log({ email, password, name });
   };
 
@@ -18,19 +58,28 @@ export default function Ex01() {
           placeholder="email"
           value={email}
           onChange={handleEmailChange}
+          // pattern="^[a-zA-Z0-9._%+\-]+@(gmail\.com|naver\.com)$"
+          required
         />
+        {emailError && <p>{emailError}</p>}
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={handlePasswordChange}
+          required
+          // minLength={3}
+          // maxLength={8}
         />
+        {passwordError && <p>{passwordError}</p>}
         <input
           type="text"
           placeholder="name"
           value={name}
           onChange={handleNameChange}
+          required
         />
+        {nameError && <p>{nameError}</p>}
         <button type="submit">제출</button>
       </form>
     </>
