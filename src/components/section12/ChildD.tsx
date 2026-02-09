5794dbbcd59a8480485b74ec1e9839a018291ad4
jelee <lje7128@mteletec.com>
2026-02-09 15:44:51 +0900

    ErrorBoundary

--- src/components/section12/ChildD.tsx
+++ src/components/section12/ChildD.tsx
@@ -1,5 +1,8 @@
 export default function ChildD() {
-  console.log("ChildD");
+  const randomNum = Math.floor(Math.random() * 2) + 1; // 1 ~ 2
+  if (randomNum === 1) {
+    throw new Error("random number is 1");
+  }
   return (
     <>
       <h1>ChildD Component</h1>
--- src/components/section12/Ex03.tsx
+++ src/components/section12/Ex03.tsx
@@ -1,10 +1,27 @@
-import React, { lazy, Suspense, useState } from "react"
+import React, { lazy, Suspense, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 // import ChildD from "./ChildD";
 // import ChildE from "./ChildE";
 
 // 필요할때 컴포넌트를 번들에 포함시켜 다운로드 가능하도록 함. 초기 앱의 로딩 성능을 향상시킬 수 있음
-const ChildD = React.lazy(() => import("./ChildD")) 
-const ChildE = lazy(() => import("./ChildE"))
+const ChildD = React.lazy(() => import("./ChildD"));
+const ChildE = lazy(() => import("./ChildE"));
+
+function Fallback({
+  error,
+  resetErrorBoundary,
+}: {
+  error: Error;
+  resetBoundary: () => void;
+}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong!</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>retry</button>
+    </div>
+  );
+}
 
 export default function Ex03() {
   const [isShow, setIsShow] = useState(false);
@@ -13,15 +30,20 @@ export default function Ex03() {
       <button onClick={() => setIsShow((isShow) => !isShow)}>토글</button>
       {isShow && (
         <>
-          {/* Suspense : 아직 준비 안 된 컴포넌트를 기다리는 동안 대신 보여줄 화면 */}
-          <Suspense fallback={<h1>ChildD Loading...</h1>}> 
-            <ChildD />
-          </Suspense>
-          <Suspense fallback={<h1>ChildE Loading...</h1>}>
-            <ChildE />
-          </Suspense>
+          {/* 컴포넌트 렌더링 중 에러가 터졌을 때 앱 전체가 죽지 않게 막아주는 안전장치 (에러 발생 시 보여줄 화면) */}
+          <ErrorBoundary FallbackComponent={Fallback}>
+            {/* Suspense : 아직 준비 안 된 컴포넌트를 기다리는 동안 대신 보여줄 화면 */}
+            <Suspense fallback={<h1>ChildD Loading...</h1>}>
+              <ChildD />
+            </Suspense>
+          </ErrorBoundary>
+          <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <Suspense fallback={<h1>ChildE Loading...</h1>}>
+              <ChildE />
+            </Suspense>
+          </ErrorBoundary>
         </>
       )}
     </>
-  )
+  );
 }
