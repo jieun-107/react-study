7e8ca1aeb8b957c77b95f0653fe785f95ce6b308
jelee <lje7128@mteletec.com>
2026-02-09 09:57:12 +0900

    useCallback

--- src/components/section12/Ex01.tsx
+++ src/components/section12/Ex01.tsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import ChildA from "./ChildA";
 
 export default function Ex01() {
   const [count, setCount] = useState(0);
+  const increment = useCallback(() => {
+    setCount((count) => count + 1);
+  }, []);
   return (
     <>
       <h1>count: {count}</h1>
       <button onClick={() => setCount((count) => count + 1)}>증가</button>
-      <ChildA />
+      <ChildA increment={increment} />
     </>
   )
 }
